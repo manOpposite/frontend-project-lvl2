6fa292b8118ff234596ea88d17acd98d7c0ceb67
Denis B <sinoptikgad@mail.ru>
2022-08-17 20:21:23 +0300

    update __tests__

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -1,10 +1,11 @@
 import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
 import { test, expect } from '@jest/globals';
+import { fileURLToPath } from 'url';
+import path from 'path';
 import genDiff from '../src/index.js';
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const firstFile = fs.readFileSync(getFixturePath('file1.json'), 'utf-8');
 const secondFile = fs.readFileSync(getFixturePath('file2.json'), 'utf-8');
